96d9cf100351917979da04cb28102e59de5d57e7
damola adebayo <adebayo10k@gmail.com>
2022-08-01 21:08:47 +0100

    add file to reference a runtime configuration file

---
+++ includes/config.inc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# path to a runtime configuration file.
+
+#config_file_fullpath="${command_dirname}/work.config.json"
+
+#config_file_fullpath="${command_dirname}/not-work.config.json"
+
+config_file_fullpath="${command_dirname}/profiles.a10k.config.json"
+
